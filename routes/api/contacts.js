6d19ff7878b3b9bf9be899a35f2928d6f90f6f35
Aleksandr <a0933093518@gmail.com>
2021-10-10 21:14:20 +0300

    на проверку

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const {
+  getContacts,
+  getContact,
+  removeContact,
+  saveContact,
+  updateContact,
+  updateStatusFavoriteContact
+} = require('../../controllers/contacts')
+const { validateContact, validateContactId, validateStatusContact } = require('./validation')
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', getContacts)
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', validateContactId, getContact)
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateContact, saveContact)
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', validateContactId, removeContact)
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:contactId', [validateContactId, validateContact], updateContact)
+
+router.patch(
+  '/:contactId/favorite/',
+  [validateContactId, validateStatusContact],
+  updateStatusFavoriteContact
+)
 
 module.exports = router
