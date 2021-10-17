27b4b4bf3f7e4f3c3a51522051b995869c277d69
Aleksandr <a0933093518@gmail.com>
2021-10-18 01:20:05 +0300

    настраивает безопасность

--- config/constants.js
+++ config/constants.js
@@ -14,7 +14,9 @@ const HttpCode = {
     FORBIDDEN: 403,
     NOT_FOUND: 404,
     CONFLICT: 409,
+    TOO_MANY_REQUEST: 429,
     INTERNAL_SERVER_ERROR: 500,
+    
   }
 
 module.exports = {
--- routes/users/users.js
+++ routes/users/users.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const { signup, login, logout, currentUser, updateSub } = require('../../controllers/users')
 const { validateUser, validateUpdateSub} = require('./validation')
 const guard = require('../../helpers/guard')
+const loginLimit  = require('../../helpers/rate-limit-login')
 
 router.post('/signup',validateUser, signup)
-router.post('/login',validateUser, login)
+router.post('/login',validateUser, loginLimit, login)
 router.post('/logout', guard, logout)
 router.get("/current", guard, currentUser)
 router.patch("/", guard, validateUpdateSub, updateSub);
