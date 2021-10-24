6d75c7ecc025ae978b300a437f97179d5c09f69c
Aleksandr <a0933093518@gmail.com>
2021-10-24 22:58:45 +0300

    test guard

---
+++ test/data/data.js
@@ -0,0 +1,19 @@
+const newContact = {
+  name: "Rozariya Polyaris",
+  email: "Rozariya@utquamvel.net",
+  phone: "(666) 888-1111"
+}
+
+const newUserForRouteContact = {
+  email: 'colobok@gmail.ua',
+  password: '123456'
+}
+
+
+
+const newUserForRouteUser = {
+    email: 'andrey_kickass@i.ua',
+    password: '123456'
+  }
+  
+  module.exports = { newContact, newUserForRouteContact, newUserForRouteUser } 
