a58947ef18db3fb317777990766a254e519edc61
Aleksandr <a0933093518@gmail.com>
2021-10-30 00:10:12 +0300

    На проверку

--- repository/users.js
+++ repository/users.js
@@ -8,6 +8,10 @@ const findByEmail = async (email) => {
   return await User.findOne({ email })
 }
 
+const findUserByVerifyToken = async (verifyToken) => {
+  return await User.findOne({ verifyToken })
+}
+
 const create = async (options) => {
   const user = new User(options)
   return await user.save()
@@ -17,6 +21,11 @@ const updateToken = async (id, token) => {
   return await User.updateOne({ _id: id }, { token })
 }
 
+const updateTokenVerify = async (id, isVerified, verifyToken) => {
+  return await User.updateOne({ _id: id }, { isVerified, verifyToken })
+}
+
+
 const updateSubUser = async (id, subscription) => {
   return await User.updateOne({ _id: id }, { subscription });
 }
@@ -25,4 +34,12 @@ const updateAvatar = async (id, avatar, idUserCloud = null) => {
   return await User.updateOne({ _id: id }, { avatar, idUserCloud })
 }
 
-module.exports = { findById, findByEmail, create, updateToken, updateSubUser, updateAvatar }
+module.exports = { findById, 
+                   findByEmail, 
+                   create, 
+                   updateToken, 
+                   updateSubUser, 
+                   updateAvatar,
+                   updateTokenVerify,
+                   findUserByVerifyToken
+                  }
